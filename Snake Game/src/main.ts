7f72b091d3c8d616f13a64114d36b1768e9687ea
rubayatrifat <rubayatislam17@gmail.com>
2026-07-23 16:53:53 +0600

    feat: grid coordinate added

--- Snake Game/src/main.ts
+++ Snake Game/src/main.ts
@@ -1,21 +1,33 @@
-const gameBoard = document.querySelector("#gameBoard") as HTMLElement;
-const gridSize = 15
-const totalCell = gridSize * gridSize
-
-if (!gameBoard) {
-  throw new Error("Element not found in DOM!");
+// Element validation
+function getElement<T extends HTMLElement>(selector: string) {
+  const element = document.querySelector<T>(selector);
+  if (!element) {
+    throw new Error(`Element not found ${selector}`);
+  }
+  return element;
 }
 
-gameBoard.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-gameBoard.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+// Necessary HTML elements
+const gameBoard = getElement<HTMLDivElement>("#gameBoard");
 
+
+// Setting up the grid in css
+const gridCellCount = 15;
+gameBoard.style.gridTemplateColumns = `repeat(${gridCellCount}, 1fr)`;
+gameBoard.style.gridTemplateRows = `repeat(${gridCellCount}, 1fr)`;
+
+// Loading the grid
 const fragment = document.createDocumentFragment();
+const cellCoordinate: Record<string, HTMLDivElement> = {};
 
-for(let i = 0; i < totalCell; i++) {
+for (let row = 0; row < gridCellCount; row++) {
+  for (let column = 0; column < gridCellCount; column++) {
     const singleCell = document.createElement("div");
     singleCell.classList.add("cell");
-
+    cellCoordinate[`${row}-${column}`] = singleCell;
     fragment.appendChild(singleCell);
+  }
 }
- 
-gameBoard.appendChild(fragment)
+
+console.log(cellCoordinate);
+gameBoard.appendChild(fragment);
