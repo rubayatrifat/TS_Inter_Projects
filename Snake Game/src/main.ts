c8773a5f5de47d427c7b96cf7d4a528adb22c43e
rubayatrifat <rubayatislam17@gmail.com>
2026-07-23 21:39:21 +0600

    feat: snake movement add

--- Snake Game/src/main.ts
+++ Snake Game/src/main.ts
@@ -3,6 +3,7 @@ interface snakeInter {
   x: number;
   y: number;
 }
+type Direction = "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight";
 
 // *** 2. HELPER FUNCTIONS & UTILITIES ***
 function getElement<T extends HTMLElement>(selector: string): T {
@@ -14,7 +15,9 @@ function getElement<T extends HTMLElement>(selector: string): T {
 }
 
 // *** 3. CONSTANTS & CONFIGURATIONS ***
+
 const gridCellCount = 15;
+let snakeDirection = "ArrowDown" as Direction;
 
 // *** 4. DOM TARGETS & GLOBAL STATE ***
 const gameBoard = getElement<HTMLDivElement>("#gameBoard");
@@ -22,11 +25,7 @@ const cellCoordinate: Record<string, HTMLDivElement> = {};
 const cellsFragment = document.createDocumentFragment();
 
 // State
-const snake: snakeInter[] = [
-  { x: 5, y: 3 },
-  { x: 6, y: 3 },
-  { x: 7, y: 3 }
-];
+const snake: snakeInter[] = [{ x: 5, y: 3 }];
 
 // *** 5. DOM INITIALIZATION & RENDER LOGIC ***
 gameBoard.style.gridTemplateColumns = `repeat(${gridCellCount}, 1fr)`;
@@ -37,6 +36,7 @@ for (let row = 0; row < gridCellCount; row++) {
   for (let column = 0; column < gridCellCount; column++) {
     const singleCell = document.createElement("div");
     singleCell.classList.add("cell");
+    singleCell.textContent = `${row}-${column}`;
     cellCoordinate[`${row}-${column}`] = singleCell;
     cellsFragment.appendChild(singleCell);
   }
@@ -44,6 +44,8 @@ for (let row = 0; row < gridCellCount; row++) {
 gameBoard.appendChild(cellsFragment);
 
 // *** 6. GAME LOGIC FUNCTIONS ***
+
+// Render Snake
 function renderSnake(snakeSegments: snakeInter[]) {
   snakeSegments.forEach((segment) => {
     const renderingPos = `${segment.y}-${segment.x}`;
@@ -53,6 +55,43 @@ function renderSnake(snakeSegments: snakeInter[]) {
     }
   });
 }
+
+// Move the snake
+const snakeMovement = setInterval(() => {
+  const snakeHead = snake[0];
+
+  if (snake.length > 0 && snakeHead) {
+    let dx = 0;
+    let dy = 0;
+
+    switch (snakeDirection) {
+      case "ArrowUp":
+        dy = -1;
+        break;
+      case "ArrowDown":
+        dy = 1;
+        break;
+      case "ArrowRight":
+        dx = 1;
+        break;
+      case "ArrowLeft":
+        dx = -1;
+        break;
+    }
+
+    snake.unshift({
+      x: snakeHead.x + dx,   
+      y: snakeHead.y + dy,
+    });
+    const snakeTail = snake.pop();
+    if (snakeTail) {
+      const tailPos = `${snakeTail.y}-${snakeTail.x}`;
+      cellCoordinate[tailPos]?.classList.remove("snake");
+    }
+  }
+
+  renderSnake(snake);
+}, 400);
 
 // *** 7. INITIAL EXECUTION ***
 renderSnake(snake);
